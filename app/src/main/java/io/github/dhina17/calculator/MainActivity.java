f3cf0b7dfdf8df249f828eca7d0fbe3e3fcd75cd
Dhina17 <dhinalogu@gmail.com>
2021-03-16 03:02:32 +0530

    main: Auto Size the text of the calculation view
    - Rename variables as well as to follow standards.

--- app/src/main/java/io/github/dhina17/calculator/MainActivity.java
+++ app/src/main/java/io/github/dhina17/calculator/MainActivity.java
@@ -1,42 +1,55 @@
 package io.github.dhina17.calculator;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewCompat;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView calculationView;
-    private StringBuffer calculationString;
+    private TextView mCalculationView;
+    private StringBuffer mCalculationString;
+
+    private static final int AUTO_SIZE_MIN_TEXT_SIZE = 12;
+    private static final int AUTO_SIZE_MAX_TEXT_SIZE = 50;
+    private static final int AUTO_SIZE_STEP_GRANULARITY = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calculationView = findViewById(R.id.calculation_view);
-        calculationString = new StringBuffer();
+        mCalculationView = findViewById(R.id.calculation_view);
+
+        /* Auto Size the text  */
+        TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(mCalculationView,
+                AUTO_SIZE_MIN_TEXT_SIZE, AUTO_SIZE_MAX_TEXT_SIZE, AUTO_SIZE_STEP_GRANULARITY,
+                TypedValue.COMPLEX_UNIT_SP);
+
+        mCalculationString = new StringBuffer();
     }
 
     public void onButtonClick(View view) {
         MaterialButton button = (MaterialButton) view;
-        calculationString.append(button.getText());
-        updateCalculationView(calculationString);
+        mCalculationString.append(button.getText());
+        updateCalculationView(mCalculationString);
     }
 
     public void onDelButtonClick(View view) {
-        int len = calculationString.length();
+        int len = mCalculationString.length();
         if (len > 0) {
-            calculationString.deleteCharAt(len - 1);
-            updateCalculationView(calculationString);
+            mCalculationString.deleteCharAt(len - 1);
+            updateCalculationView(mCalculationString);
         }
     }
 
     private void updateCalculationView(StringBuffer sb) {
-        calculationView.setText(sb);
+        mCalculationView.setText(sb);
     }
 
 }
